6d2befb192cb2864b05832775f8a343a55048163
Jayakrishna-1817 <jksmart1817@gmail.com>
2026-01-11 17:03:05 +0530

    Fix vite config - add missing default keyword

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { copyFileSync } from "fs";
 
 // https://vitejs.dev/config/
-export defineConfig(() => ({
+export default defineConfig(() => ({
   base: "/",
   build: {
     outDir: "dist",
